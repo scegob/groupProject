02482fc1162a7190da2f61f51a257e59a5d49bcb
Billeh <scegob@uw.edu>
2024-05-16 02:36:11 -0700

    got the array to work

--- index.js
+++ index.js
@@ -1,53 +1,43 @@
+// data for the array url https://www.polygon.com/23648669/best-video-games-2023 -Bill
+
 const gamesData = {
   The_Legend_Of_Zelda: {
     publisherName: "Nintendo EPD",
     name: "The Legends of Zelda: Tears of the kingdom",
-    platforms: { wins: 7, losses: 2 },
+    platforms: "Nintendo Switch",
     rating: "1",
   },
   Baldurs_Gate_3: {
     publisherName: "Larian Studio",
-    name: "Baldur{`$'s`} Gate 3",
-    platform: { wins: 6, losses: 3 },
-    overallRecord: { wins: 7, losses: 6 },
+    name: "Baldur's Gate 3",
+    platform: "PlayStation 5 and Windows PC",
+    rating: "2",
   },
-  OREGON_STATE: {
-    schoolId: "OSU",
-    logoImage: "Components/Images/osu.png",
-    name: "OREGON STATE",
-    conferenceRecord: { wins: 5, losses: 4 },
+  Alan_Wake_2: {
+    publisherName: "OSU",
+    name: "ALAN WAKE 2",
+    platform: "PlayStation 5, Windows PC, and Xbox Series X",
+    rating: "3",
   },
-  CALIFORNIA: {
-    schoolId: "CAL",
-    logoImage: "Components/Images/cal.png",
-    name: "CALIFORNIA",
-    conferenceRecord: { wins: 4, losses: 5 },
-  },
-  WASHINGTON: {
-    schoolId: "UW",
-    logoImage: "Components/Images/uw.png",
-    name: "WASHINGTON",
-    conferenceRecord: { wins: 3, losses: 6 },
-  },
-  STANFORD: {
-    schoolId: "SU",
-    logoImage: "Components/Images/stanford.png",
-    name: "STANFORD",
-    conferenceRecord: { wins: 2, losses: 7 },
+  Super_Mario_Bros_Wonder: {
+    publisherName: "Nintendo EPD",
+    name: "ALAN WAKE 2",
+    platform: "Super Mario Bros. Wonder",
+    rating: "4",
   },
 };
 
 document.getElementById("loadDataBtn").addEventListener("click", function () {
-  const tableBody = document.querySelector("#pac12-table tbody");
+  const tableBody = document.querySelector("#games-table tbody");
   tableBody.innerHTML = "";
 
-  Object.values(gamesData).forEach(function (team) {
+  Object.values(gamesData).forEach(function (game) {
     const row = document.createElement("tr");
     row.innerHTML = `
-        <td>${team.name}</td>
-        <td>${team.conferenceRecord.wins}</td>
-        <td>${team.overallRecord.wins}</td>
-        <td>${team.lastGame.result} ${team.lastGame.score} vs ${team.lastGame.opponent}</td>
+        <td>${game.name}</td>
+        <td>${game.publisherName}</td>
+        <td>${game.platform}</td>
+        <td>${game.rating}</td>
       `;
     tableBody.appendChild(row);
   });
