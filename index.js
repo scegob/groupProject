d370c3859a2f1f5266a77a4603736a9c52b1fc4d
Billeh <scegob@uw.edu>
2024-05-16 15:22:22 -0700

    array is done

--- index.js
+++ index.js
@@ -2,28 +2,66 @@
 
 const gamesData = {
   The_Legend_Of_Zelda: {
-    publisherName: "Nintendo EPD",
+    developerName: "Nintendo EPD",
     name: "The Legends of Zelda: Tears of the kingdom",
     platforms: "Nintendo Switch",
     rating: "1",
   },
   Baldurs_Gate_3: {
-    publisherName: "Larian Studio",
+    developerName: "Larian Studio",
     name: "Baldur's Gate 3",
     platform: "PlayStation 5 and Windows PC",
     rating: "2",
   },
   Alan_Wake_2: {
-    publisherName: "OSU",
+    developerName: "Remedy Entertainment",
     name: "ALAN WAKE 2",
     platform: "PlayStation 5, Windows PC, and Xbox Series X",
     rating: "3",
   },
   Super_Mario_Bros_Wonder: {
-    publisherName: "Nintendo EPD",
-    name: "ALAN WAKE 2",
-    platform: "Super Mario Bros. Wonder",
+    developerName: "Nintendo EPD",
+    name: "Super Mario Bros. Wonder",
+    platform: "Nintendo Switch",
     rating: "4",
+  },
+  Pikmin_4: {
+    developerName: "Nintendo EPD, Eighting",
+    name: "Pikmin 4",
+    platform: "Nintendo Switch",
+    rating: "5",
+  },
+  COCOON: {
+    developerName: "Geometric Interactive",
+    name: "Cocoon",
+    platform:
+      "Nintendo Switch, PlayStation 4, PlayStation 5, Windows PC, Xbox One, and Xbox Series X",
+    rating: "6",
+  },
+  Street_Fighter_6: {
+    developerName: "Capcom",
+    name: "Street Fighter 6",
+    platform: "PlayStation 4, PlayStation 5, Windows PC, and Xbox Series X",
+    rating: "7",
+  },
+  Marvels_Spider_Man_2: {
+    developerName: "Insomniac Games",
+    name: "Marvel’s Spider-Man 2",
+    platform: "PlayStation 5",
+    rating: "8",
+  },
+  Jusant: {
+    developerName: "Don’t Nod",
+    name: "Marvel’s Spider-Man 2",
+    platform: "PlayStation 5, Windows PC, and Xbox Series X",
+    rating: "9",
+  },
+  Armored_Core_6: {
+    developerName: "FromSoftware",
+    name: "Armored Core 6: Fires Of Rubicon",
+    platform:
+      "PlayStation 4, PlayStation 5, Windows PC, Xbox One, and Xbox Series X",
+    rating: "10",
   },
 };
 
